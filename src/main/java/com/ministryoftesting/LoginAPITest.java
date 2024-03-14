11442ab1c61ebacc8aefd2211aabb8c8b24e617d
CMurtha <cmurtha@naic.org>
2024-03-14 12:49:20 -0500

    Initial commit

---
+++ src/main/java/com/ministryoftesting/LoginAPITest.java
@@ -0,0 +1,44 @@
+package com.ministryoftesting;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = TimesheetManagerApplication.class)
+@ActiveProfiles("dev")
+public class LoginAPITest {
+
+    @Test
+    public void testPageUpdatesToProjectPageAfterLogin() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:8080");
+
+        driver.findElement(By.name("email")).sendKeys("admin@test.com");
+        driver.findElement(By.name("password")).sendKeys("password123");
+        driver.findElement(By.cssSelector("button")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".card-title")));
+
+        String title = driver.findElement(By.cssSelector(".card-title")).getText();
+        assertEquals("Projects", title);
+
+        driver.close();
+        driver.quit();
+    }
+}
